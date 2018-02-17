a21ce207f26a8a486d3c802b6635255c6b72d88c
Andrew Larin <larin.projects@gmail.com>
2018-02-17 21:08:24 +0300

    Add hadoop mapreduce results

--- hw13/run_counter.sh
+++ hw13/run_counter.sh
@@ -3,8 +3,7 @@
 /usr/bin/hadoop jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar \
     -D mapred.reduce.tasks=3 \
     -input /tmp/words-count/alice_texts \
-    -output /tmp/words-count/alice_pairs \
+    -output /tmp/words-count/alice_pairs_count \
     -mapper 'python3 mapper.py' \
-    -reducer 'python3 reducer.py' \
-    -file mapper.py \
-    -file reducer.py
+    -reducer '/usr/bin/wc -l' \
+    -file mapper.py
--- hw13/test_run.sh
+++ hw13/test_run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cat alice.txt | ./mapper.py | sort | ./reducer.py | sort -r -n -k 2 | head -n 200 > tests/test_top_200_pairs.txt
-cat alice.txt | ./mapper.py | sort | ./reducer.py | wc -l > tests/test_pairs_count.txt
+cat alice_texts/alice_1.txt alice_texts/alice_2.txt alice_texts/alice_3.txt | ./mapper.py | sort | ./reducer.py | sort -r -n -k 2 | head -n 200 > tests/test_top_200_pairs.txt
+cat alice_texts/alice_1.txt alice_texts/alice_2.txt alice_texts/alice_3.txt | ./mapper.py | wc -l > tests/test_pairs_count.txt
